5d7ff969b25016f30ecc44d7b8cbd080b6f4de26
Yan1889 <yan1889v2@gmail.com>
2026-06-13 17:35:21 +0200

    Initial Commit

---
+++ plotter-3d/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  plotter-3d
+//
+//  Created by Yan Amin on 13.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ plotter-3d/plotter_3dApp.swift
@@ -0,0 +1,17 @@
+//
+//  plotter_3dApp.swift
+//  plotter-3d
+//
+//  Created by Yan Amin on 13.06.26.
+//
+
+import SwiftUI
+
+@main
+struct plotter_3dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
