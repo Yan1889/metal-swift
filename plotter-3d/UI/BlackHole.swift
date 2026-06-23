fd3ac87ab8bfd07383ad48a04bb95e9892bbade7
Yan1889 <yan1889v2@gmail.com>
2026-06-23 17:18:59 +0200

    black hole template

---
+++ plotter-3d/UI/BlackHole.swift
@@ -0,0 +1,14 @@
+//
+//  BlackHole.swift
+//  plotter-3d
+//
+//  Created by Yan Amin on 23.06.26.
+//
+
+import SwiftUI
+
+struct BlackHole: View {
+    var body: some View {
+        Circle()
+    }
+}
--- plotter-3d/UI/ContentView.swift
+++ plotter-3d/UI/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var isBlackHole = false
+    
     @State var last_point: CGPoint?
     
     @State var settings = Settings(
@@ -27,25 +29,36 @@ struct ContentView: View {
         )
     )
     
+    var switchText: String {
+        isBlackHole ? "function plotter" : "black hole simulation"
+    }
+    
     var body: some View {
-        HStack {
-            SettingsPanel(settings: $settings)
-                .frame(width: 500)
-                .padding()
-            
-            MetalView(settings: $settings)
-                .frame(width: 800, height: 800)
-                .gesture(
-                    DragGesture()
-                        .onChanged { e in
-                            settings.pull.cam_yaw -= Float((e.location.x - (last_point ?? e.startLocation).x) / 900.0)
-                            settings.pull.cam_pitch += Float((e.location.y - (last_point ?? e.startLocation).y) / 900.0)
-                            last_point = e.location
-                        }
-                        .onEnded { _ in
-                            last_point = nil
-                        }
-                )
+        Button("Switch to \(switchText)") {
+            isBlackHole.toggle()
+        }
+        if isBlackHole {
+            BlackHole()
+        } else {
+            HStack {
+                SettingsPanel(settings: $settings)
+                    .frame(width: 500)
+                    .padding()
+                
+                MetalView(settings: $settings)
+                    .frame(width: 800, height: 800)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { e in
+                                settings.pull.cam_yaw -= Float((e.location.x - (last_point ?? e.startLocation).x) / 900.0)
+                                settings.pull.cam_pitch += Float((e.location.y - (last_point ?? e.startLocation).y) / 900.0)
+                                last_point = e.location
+                            }
+                            .onEnded { _ in
+                                last_point = nil
+                            }
+                    )
+            }
         }
     }
 }
