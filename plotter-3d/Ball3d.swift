a2404920cbc9722d94fa0c6d2267d478605a6db3
Yan1889 <yan1889v2@gmail.com>
2026-06-24 17:47:00 +0200

    balls; move matrix helper functions into own file `Utils.swift`

--- plotter-3d/Ball3d.swift
+++ plotter-3d/Ball3d.swift
@@ -15,7 +15,60 @@ class Ball3d {
     private var radius: Float
     private var pos: SIMD4<Float>
     
-    init(resolution: Int) {
-        fatalError()
+    init(radius: Float, pos: SIMD4<Float>, color: SIMD4<Float>, resolution: Int, device: MTLDevice) {
+        self.radius = radius
+        self.pos = pos
+        
+        var vertices: [Vertex] = []
+        var indices: [UInt32] = []
+        
+        for i in 0..<UInt32(resolution) {
+            for j in 0..<UInt32(resolution) {
+                let long = Float(i) / Float(resolution - 1) * .pi * 2
+                let lat  = Float(j) / Float(resolution - 1) * .pi
+                
+                let x = sin(lat) * cos(long)
+                let y = cos(lat)
+                let z = sin(lat) * sin(long)
+                
+                vertices.append(Vertex(
+                    pos: SIMD4<Float>(x, y, z, 1),
+                    col: color,
+                ))
+                
+                let r = UInt32(resolution)
+                for arr in quad_indices {
+                    indices.append(((i + arr[0]) % r) * r + j + arr[1])
+                }
+            }
+        }
+        
+        vertexBuffer = device.makeBuffer(
+            bytes: vertices,
+            length: vertices.count * MemoryLayout<Vertex>.stride
+        )!
+        
+        indexBuffer = device.makeBuffer(
+            bytes: indices,
+            length: indices.count * 4
+        )!
+    }
+    
+    func encode(encoder: MTLRenderCommandEncoder, projection_view: float4x4) {
+        let S = make_scalation_matrix(radius, radius, radius)
+        let R = matrix_identity_float4x4
+        let T = make_translation_matrix(pos.x, pos.y, pos.z)
+        let model = T * R * S
+        var mvp = projection_view * model
+        
+        encoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        encoder.setVertexBytes(&mvp, length: 4 * 4 * 4, index: 1)
+        encoder.drawIndexedPrimitives(
+            type: .triangle,
+            indexCount: indexBuffer.length / 4,
+            indexType: .uint32,
+            indexBuffer: indexBuffer,
+            indexBufferOffset: 0,
+        )
     }
 }
--- plotter-3d/Renderer.swift
+++ plotter-3d/Renderer.swift
@@ -5,7 +5,6 @@
 //  Created by Yan Amin on 13.06.26.
 //
 
-import Foundation
 import MetalKit
 import SwiftUI
 
@@ -395,6 +394,16 @@ class Renderer: NSObject, MTKViewDelegate {
             l.encode(encoder: encoder, projection_view: projection_view)
         }
         
+        let s = Ball3d(
+            radius: 0.3,
+            pos: SIMD4<Float>(0, 0, 0, 1),
+            color: SIMD4<Float>(0, 1, 0, 1),
+            resolution: 100,
+            device: device
+        )
+        
+        s.encode(encoder: encoder, projection_view: projection_view)
+        
         encoder.setVertexBuffer(vertexBuffer_x_z_plane, offset: 0, index: 0)
         encoder.setVertexBytes(&mvp, length: 4 * 4 * 4, index: 1)
         encoder.drawIndexedPrimitives(
@@ -408,62 +417,6 @@ class Renderer: NSObject, MTKViewDelegate {
         encoder.endEncoding()
         commandBuffer.present(draw)
         commandBuffer.commit()
-    }
-    
-    func make_translation_matrix(_ x: Float, _ y: Float, _ z: Float) -> matrix_float4x4 {
-        return matrix_float4x4(
-            SIMD4<Float>(1, 0, 0, 0),
-            SIMD4<Float>(0, 1, 0, 0),
-            SIMD4<Float>(0, 0, 1, 0),
-            SIMD4<Float>(x, y, z, 1),
-        )
-    }
-    
-    func make_rotation_matrix(_ angle: Float, axis: SIMD3<Float>) -> matrix_float4x4 {
-        matrix_float4x4(simd_quatf(angle: angle, axis: simd_normalize(axis)))
-    }
-    
-    func make_perspective_matrix(fovY: Float,
-                                 aspect: Float,
-                                 near: Float,
-                                 far: Float) -> simd_float4x4 {
-        
-        let yScale = 1 / tan(fovY * 0.5)
-        let xScale = yScale / aspect
-        
-        let zRange = far - near
-        let zScale = -(far + near) / zRange
-        let wzScale = -(2 * far * near) / zRange
-        
-        return simd_float4x4(
-            SIMD4<Float>(xScale, 0,      0,  0),
-            SIMD4<Float>(0,      yScale, 0,  0),
-            SIMD4<Float>(0,      0,      zScale, -1),
-            SIMD4<Float>(0,      0,      wzScale, 0)
-        )
-    }
-    
-    func lookAt(
-        eye: SIMD3<Float>,
-        center: SIMD3<Float>,
-        up: SIMD3<Float>
-    ) -> simd_float4x4 {
-        
-        let f = simd_normalize(center - eye)
-        let s = simd_normalize(simd_cross(f, up))
-        let u = simd_cross(s, f)
-        
-        return simd_float4x4(
-            SIMD4<Float>( s.x,  u.x, -f.x, 0),
-            SIMD4<Float>( s.y,  u.y, -f.y, 0),
-            SIMD4<Float>( s.z,  u.z, -f.z, 0),
-            SIMD4<Float>(
-                -simd_dot(s, eye),
-                 -simd_dot(u, eye),
-                 simd_dot(f, eye),
-                 1
-            )
-        )
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
